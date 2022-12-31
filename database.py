57a194df3afd22b88abf3027b5ff2f271fa9d08d
kanngji <alleneffort1994@gmail.com>
2022-12-31 22:36:26 +0900

    라우터 , 의존성 주입

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -13,3 +15,12 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
